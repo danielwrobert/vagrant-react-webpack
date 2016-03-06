ad75e69c80c3d8a9bc8b0ed708a18d431050f7db
Daniel W. Robert <danielwrobert@gmail.com>
2016-03-06 17:51:45 -0500

    Updated packages and loaders to include Sass compilation.

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,45 @@
-var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
 
 module.exports = {
   entry: {
-    'vagrant-react-webpack': [
+    app: [
       'webpack-dev-server/client?http://localhost:8881/',
       './app/App.js'
     ]
   },
+
   output: {
-    filename: 'public/main.js',
+    filename: 'public/js/main.js',
     sourceMapFilename: 'main.js.map'
   },
+
   resolve: {
     extensions: ['', '.js', '.jsx', '.es6'],
     modulesDirectories: ['node_modules']
   },
+
   module: {
+    preloaders: [
+      { test: /\.jsx$|\.es6$|\.js$/, loader: 'source-map', query: { presets: ['react', 'es2015'] }, exclude: /(node_modules|bower_components)/ }
+    ],
     loaders: [
       { test: /\.jsx$|\.es6$|\.js$/, loader: 'babel', query: { presets: ['react', 'es2015'] }, exclude: /(node_modules|bower_components)/ },
-      { test: /\.scss$|\.css$/, loader: 'style-loader!style!css!sass' }
+      { test: /\.scss$|\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader!sass-loader') },
+      { test: /\.jpe?g$|\.png$|\.gif$|\.svg$/, loader: 'url?limit=8192!img' }
     ]
   },
+
+  devtool: "eval",
+
+  postcss: function() {
+    return [autoprefixer, precss];
+  },
+
   plugins: [
-    new webpack.NoErrorsPlugin()
-  ],
-  devtool: "eval-source-map"
+    new ExtractTextPlugin( 'public/styles.css', {
+      allChunks: true
+    } )
+  ]
 };
