49ee6c8f37f5e9e9eaa16ecd615255d69204875d
Daniel W. Robert <danielwrobert@gmail.com>
2016-08-31 13:58:38 -0400

    Updated config so CSS can be output in both dev and production.
    - Set output path to root dir
    - Removed Bootstrap CSS link
    - Removed container div and kept app target div
    - Changed target div ID from 'react' to 'app'
    - Note: Production bundle will now place styles.css and main.js in root dir

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
 
   output: {
     path: path.resolve( __dirname, 'public' ),
-    publicPath: '/js/',
+    publicPath: '/',
     filename: 'main.js',
     sourceMapFilename: 'main.js.map'
   },
